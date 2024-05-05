3f0171c00b4555bfa0f5372697d381467054f32e
Lukas Huber <lukashuber2020@gmail.com>
2024-05-05 13:40:22 +0200

    added query to test.php showing entire db table "Artikel"

--- public-html/test.php
+++ public-html/test.php
@@ -1,3 +1,29 @@
 <?php
 
-echo "Hello World!";
+echo "Hello World";
+
+$connect = mysqli_connect(
+    'db', #service name
+    'php_docker', # username
+    'password', #password
+    'php_docker' #db table
+);
+
+$tableName = "Artikel";
+
+$query = "SELECT * FROM $tableName";
+
+$respone = mysqli_query($connect, $query);
+
+echo "<strong> $tableName: </strong>";
+while($i = mysqli_fetch_assoc($respone)){
+    echo "<p>".$i['art_num']."</p>";
+    echo "<p>".$i['name']."</p>";
+    echo "<p>".$i['gender']."</p>";
+    echo "<p>".$i['price']."</p>";
+    echo "<p>".$i['size']."</p>";
+    echo "<p>".$i['color']."</p>";
+    echo "<p>".$i['category']."</p>";
+    echo "<p>".$i['image_url']."</p>";
+    echo "<br />";
+}
