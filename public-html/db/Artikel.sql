279b75148d84e670fabb8e1740af94d195b748bf
Lukas Huber <lukashuber2020@gmail.com>
2024-05-05 13:20:58 +0200

    Artikel.sql has all 10 articles ready on load

--- public-html/db/Artikel.sql
+++ public-html/db/Artikel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db:3306
--- Generation Time: May 04, 2024 at 02:32 PM
+-- Generation Time: May 05, 2024 at 11:16 AM
 -- Server version: 8.4.0
 -- PHP Version: 8.2.8
 
@@ -44,7 +44,15 @@ CREATE TABLE `Artikel` (
 
 INSERT INTO `Artikel` (`art_num`, `name`, `gender`, `price`, `size`, `color`, `category`, `image_url`) VALUES
 (1, 'Monkey Basketball shorts men', 'Männer', 20, 'S, M, L, XL, XXL', 'Grün, Violett', 'Hosen', 'https://cdn.pixabay.com/photo/2017/08/11/14/19/honor-2631271_1280.jpg\r\n'),
-(2, 'Zen Socken', 'Männer, Frauen, Unisex', 13, 'one-size', 'Violett, Grün, Orange', 'Accessories', '\"https://cdn.pixabay.com/photo/2017/08/11/14/19/honor-2631271_1280.jpg');
+(2, 'Zen Socken', 'Männer, Frauen, Unisex', 13, 'one-size', 'Violett, Grün, Orange', 'Accessories', '\"https://cdn.pixabay.com/photo/2017/08/11/14/19/honor-2631271_1280.jpg'),
+(3, 'Monkeys Polo', 'Männer', 22, 'S,M,L,XL,XXL', 'Orange,Schwarz', 'Tshirt', 'https://cdn.pixabay.com/photo/2017/08/11/14/19/honor-2631271_1280.jpg'),
+(4, 'T-Red', 'Männer,Frauen,Unisex', 20, 'S,M,L,XL,XXL', 'Rot', 'Tshirt', 'https://cdn.pixabay.com/photo/2016/12/06/09/31/blank-1886008_1280.png'),
+(5, 'Braune Herrenjacke', 'Männer', 80, 'M,L,XL,XXL', 'Braun', 'Jacke', 'https://cdn.pixabay.com/photo/2017/09/07/04/54/khaki-2723896_1280.jpg'),
+(6, 'Brilliantenarmband', 'Frauen', 30, 'S,M,L', 'Silber', 'Accessoires', 'https://cdn.pixabay.com/photo/2016/11/11/17/34/diamond-1817291_1280.png'),
+(7, 'T-Shirt Striped', 'Männer,Frauen,Unisex', 40, 'S,M,L,XL,XXL', 'Schwarz-Weiß,Rot-Blau', 'Tshirt', 'https://cdn.pixabay.com/photo/2017/08/11/14/19/honor-2631271_1280.jpg'),
+(8, 'Baggy Jeans', 'Männer', 80, 'M,L,XL,XXL', 'Black,Gray', 'Hose', 'https://cdn.pixabay.com/photo/2017/08/11/14/19/honor-2631271_1280.jpg'),
+(9, 'Hoodie mit Aufdruck', 'Männer,Frauen,Unisex', 100, 'L,XL,XXL,3XL', 'Schwarz,Grau,Dunkelblau', 'Hoodie', 'https://cdn.pixabay.com/photo/2017/08/11/14/19/honor-2631271_1280.jpg'),
+(10, 'Anzug', 'Männer', 240, 'M,L,XL', 'Schwarz,Blau,Grau', 'Anzug', 'https://cdn.pixabay.com/photo/2017/08/11/14/19/honor-2631271_1280.jpg');
 
 --
 -- Indexes for dumped tables
