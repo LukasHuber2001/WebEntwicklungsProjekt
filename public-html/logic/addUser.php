cd8d579988d299be41c18573a6acf6d92044a8c8
Lukas Huber <lukashuber2020@gmail.com>
2024-05-31 17:02:53 +0200

    addUsers works now

--- public-html/logic/addUser.php
+++ public-html/logic/addUser.php
@@ -1,21 +1,35 @@
 <?php
-
-echo $_POST['vorname'];
-echo $_REQUEST['vorname'];
-/*
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $mysqli = new mysqli("localhost", "root", '', "test");
 
-/* Prepared statement, stage 1: prepare 
-$stmt = $mysqli->prepare("INSERT INTO users(id, vorname, nachname, adresse, adresse2, ort, plz, land, username, password, isAdmin) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, NULL)");
+// Prepared statement, stage 1: prepare 
+$stmt = $mysqli->prepare("INSERT INTO users(id, vorname, nachname, adresse, adresse2, ort, plz, land, username, password, isAdmin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-/* Prepared statement, stage 2: bind and execute 
+// Prepared statement, stage 2: bind and execute 
 $id = NULL;
-$vorname, nachname, adresse, adresse2, ort, plz, land, username, password, isAdmin
+$vorname = $_POST['vorname'];
+$nachname = $_POST['nachname'];
+$adresse = $_POST['adresse'];
+$adresse2 = $_POST['adresse2'];
+$ort = $_POST['ort'];
+$plz = $_POST['plz'];
+$land = $_POST['land'];
+$username = $_POST['username']; 
+$password = $_POST['password'];
+$isAdmin = NULL;
 
-$stmt->bind_param($id, $label); // "is" means that $id is bound as an integer and $label as a string
+$stmt->bind_param('sssssssssss', $id, $vorname, $nachname, $adresse, $adresse2, $ort, $plz, $land, $username, $password, $isAdmin); // "is" means that $id is bound as an integer and $label as a string
 
 $stmt->execute();
 
-*/
+$filename = "../data/users.json";
+$sel = "SELECT * FROM users";
+$result = $mysqli->query($sel);
+if($result->num_rows>0){
+    
+    $rows = $result->fetch_all(MYSQLI_ASSOC);
+    $rowsAsJSON = json_encode($rows);
+    file_put_contents("../data/users.json", $rowsAsJSON);
+}
+
 ?>
