4a42df783916c1b25e1a8de9002057bfa2ac58c4
Lukas Huber <lukashuber2020@gmail.com>
2024-06-12 16:04:59 +0200

    prepared stmt + page redirect + email verification (register finished)

--- public-html/backend/logic/register.php
+++ public-html/backend/logic/register.php
@@ -1,40 +1,53 @@
 <?php
-echo 'does this link work?';
+
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $mysqli = new mysqli("localhost", "root", '', "test");
 mysqli_set_charset($mysqli, "utf8");
 
-if(isset($_GET['register'])) {
+if(isset($_POST)) {
+    echo 'does this link work?';
     $error = false;
-    $email = $_POST['username'];
+    $email = $_POST['email'];
     $password = $_POST['password'];
     $password2 = $_POST['password2'];
     $username = $_POST['username'];
 
-    if(strlen($passwort) == 0) {
+    if(strlen($username) == 0) {
+        echo 'Bitte einen Username angeben<br>';
+        $error = true;
+    }
+    //Überprüfen, dass ein passwort gesetzt wurde
+    if(strlen($password) == 0) {
         echo 'Bitte ein Passwort angeben<br>';
         $error = true;
     }
-    if($passwort != $passwort2) {
+    //Überprüfen, dass beide Passwörter ident sind
+    if($password != $password2) {
         echo 'Die Passwörter müssen übereinstimmen<br>';
         $error = true;
     }
+    // Überpüfen ob die Email das richtige Format hat
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error = true;
+        echo 'Falsches email Format<br>';
+    }
     //Überprüfe, dass die E-Mail-Adresse noch nicht registriert wurde
     if(!$error) { 
-        $statement = $pdo->prepare("SELECT * FROM users WHERE username = :username");
-        $result = $statement->execute(array('username' => $username));
-        $user = $statement->fetch();
+        $stmt = $mysqli-> prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
         
-        if($user !== false) {
-            echo 'Der Benutzername ist bereits vergeben<br>';
+        if($mysqli->affected_rows != 0) {
+            echo 'Die Emailadresse ist bereits vergeben<br>';
             $error = true;
-        }    
+        }
     }
     if(!$error) {    
         $password_hash = password_hash($password, PASSWORD_DEFAULT);
         
         // Prepared statement, stage 1: prepare 
-        $stmt = $mysqli->prepare("INSERT INTO users(id, vorname, nachname, adresse, adresse2, ort, plz, land, username, password, isAdmin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $statement = $mysqli->prepare("INSERT INTO users(id, vorname, nachname, adresse, adresse2, ort, plz, land, username, password, isAdmin, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
         // Prepared statement, stage 2: bind and execute 
         $id = NULL;
@@ -48,9 +61,9 @@ if(isset($_GET['register'])) {
         $username = $_POST['username']; 
         $isAdmin = NULL;
         
-        $stmt->bind_param('sssssssssss', $id, $vorname, $nachname, $adresse, $adresse2, $ort, $plz, $land, $username, $password_hash, $isAdmin); // "is" means that $id is bound as an integer and $label as a string
+        $statement->bind_param('ssssssssssss', $id, $vorname, $nachname, $adresse, $adresse2, $ort, $plz, $land, $username, $password_hash, $isAdmin, $email); // "s" means that $id is bound as an integer and $label as a string
 
-        $stmt->execute();
+        $statement->execute();
 
         $filename = "../data/users.json";
         $sel = "SELECT * FROM users";
@@ -61,6 +74,7 @@ if(isset($_GET['register'])) {
             $rowsAsJSON = json_encode($rows);
             file_put_contents("../data/users.json", $rowsAsJSON);
         }   
+        header("Location: http://localhost/webentwicklungsprojekt/public-html/frontend/sites/impressum.html");
+        die();
 }
 }
-?>
