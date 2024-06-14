7b753d786b067b76a7dc6dfa6acfeec44282d2c8
Lukas Huber <lukashuber2020@gmail.com>
2024-06-14 20:04:32 +0200

    register login and logout in 1 file. deleted old files

--- public-html/backend/logic/accountLogic.php
+++ public-html/backend/logic/accountLogic.php
@@ -115,8 +115,15 @@ class profileLogic{
         // Wenn der Benutzer erfolgreich in die Datenbank hinzugefügt wurde, dass erscheint die Meldung, dass ein/e neue/r BenutzerIn erstellt wurde
         // eine entsprechende Meldung erscheint. Andernfalls bedeutet es dass der eingegebene Username bereits existiert und diese Meldunge erscheint auch.
         // if executed and a row affected return success message, else return error message
+        // if the statement executes sucessfully the entire database table is updated into the JSON-file
         if ($stmt->execute() && $stmt->affected_rows > 0) {
             $result['success'] = 'Neuer Benutzer erstellt!';
+            $sel = "SELECT * FROM users";
+            $resultJson = $this->dh->db_obj->query($sel);
+            $rows = $resultJson->fetch_all(MYSQLI_ASSOC);
+            $rowsAsJSON = json_encode($rows);
+            file_put_contents("../data/users.json", $rowsAsJSON);
+    
         } else {
             $result['error'] = 'Benutzername existiert bereits!';
         }
@@ -196,5 +203,203 @@ class profileLogic{
 
         $stmt->close();
         return $result;
+    }
+
+     // wenn man seine eigenen Daten ändern will, wird zuerst die Methode getProfileData aufgerufen, welche die alten Daten anzeigt
+     function getProfileData($param)
+     {
+ 
+         $result = array();
+         // Wenn die Connection nicht erfolgreich war
+         if (!$this->dh->checkConnection()) {
+             $result['error'] = "Versuchen Sie es später erneut";
+             return;
+         }
+         // Man holt sich die Daten des Users aus der Datenbank
+         $sql = 'SELECT `vorname`, `nachname`, `adresse`, `plz`, `ort`, `email`, `username`
+         FROM `users` WHERE `username` = ?';
+         $stmt = $this->dh->db_obj->prepare($sql);
+         $stmt->bind_param('s', $param);
+         if ($stmt->execute()) {
+             $queryResult = $stmt->get_result();
+             // wenn es einen Eintrag in der Datenbank gibt
+             if ($queryResult->num_rows == 1) {
+                 $result = $queryResult->fetch_assoc();
+             } else {
+                 // wenn es keinen Eintrag in der Datenbank gibt
+                 $result['error'] = "Fehler bei der Abfrage";
+             }
+         } else {
+             $result['error'] = "Fehler bei der Abfrage";
+         }
+ 
+         $stmt->close();
+         return $result;
+     }
+
+     // wenn der User seine Daten ändern will
+    function updateUserData($param)
+    {
+        // wenn der User seine Daten ändern will, dann muss er davor einige Voraussetzungen erfüllen, wie zum Beispiel, dass das Passwort
+        // des Users/ der Userin eingetragen werden muss...
+        $result = array();
+        //man holt sich erneut die Daten des users, wo sie dem Usernamen entsprechen
+        $sql = 'SELECT vorname, nachname, adresse, plz, ort, email, username, passwort FROM users WHERE username = ?';
+        $stmt = $this->dh->db_obj->prepare($sql);
+        $stmt->bind_param('s', $param['actualusername']);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows == 1) {
+
+            $row = $result->fetch_assoc();
+            if (empty($param['pw_alt'])) {
+                // wenn man das alte Passwort nicht angegeben hat
+                $data['error'] = "Sie haben Ihr altes Passwort nicht eingegeben.";
+                return $data;
+            } elseif (!password_verify($param['pw_alt'], $row['password'])) {
+                // wenn man ein falsches, altes Passwort eingegeben hat
+                $data['error'] = "Das eingegebene Passwort ist nicht korrekt. Bitte probieren Sie es noch einmal.";
+                return $data;
+            } else {
+                // wenn man Daten ausfüllt, wird das in der Variable Data gespeichert,
+                // ansonsten bleibt data gleich, wie die alten Daten aus der Datenbank
+                if (!empty($param['vorname'])) {
+                    $data['vorname'] = $param['vorname'];
+                } else {
+                    $data['vorname'] = $row['vorname'];
+                }
+                if (!empty($param['nachname'])) {
+                    $data['nachname'] = $param['nachname'];
+                } else {
+                    $data['nachname'] = $row['nachname'];
+                }
+
+                if (!empty($param['adresse'])) {
+                    $data['adresse'] = $param['adresse'];
+                } else {
+                    $data['adresse'] = $row['adresse'];
+                }
+
+                if (!empty($param['plz'])) {
+                    $data['plz'] = $param['plz'];
+                } else {
+                    $data['plz'] = $row['plz'];
+                }
+
+                if (!empty($param['ort'])) {
+                    $data['ort'] = $param['ort'];
+                } else {
+                    $data['ort'] = $row['ort'];
+                }
+                // Email-Validierung
+                if (!empty($param['email']) && filter_var($this->test_input($param["email"]), FILTER_VALIDATE_EMAIL)) {
+                    $data['email'] = $param['email'];
+                } else {
+                    $data['email'] = $row['email'];
+                }
+                if (!empty($param['username'])) {
+                    $data['username'] = $param['username'];
+                } else {
+                    $data['username'] = $row['username'];
+                }
+                if (!empty($param['pw'])) {
+                    if (strlen(trim($param['pw'])) == 0 || strlen(trim($param['pw'])) < 8) {
+                        $data['error'] = "Passwort muss mindestens 8 Zeichen lang sein!";
+                        $stmt->close();
+                        return $data;
+                    } else {
+                        $data['pw'] = password_hash($param['pw'], PASSWORD_DEFAULT);
+                    }
+                } else {
+                    $data['pw'] = $row['passwort'];
+                }
+            }
+        } else {
+            $data['error'] = "Fehler bei der Abfrage!";
+            return $data;
+        }
+        $stmt->close();
+        // Heir wird nur der username aus der Datenbank gefiltert
+        $inputUsername = $param['username'];
+        $actualUsername = $param['actualusername'];
+        $sql2 = 'SELECT username FROM users WHERE `username` = ?';
+        $stmt2 = $this->dh->db_obj->prepare($sql2);
+        $stmt2->bind_param("s", $inputUsername);
+        $stmt2->execute();
+        $result2 = $stmt2->get_result();
+
+        // wenn deer neue Username bereits in der Datenbank drinnen ist, dann erscheint die Meldung, weil der Username uniqe sein muss
+        if ($result2->num_rows == 1 && $result2->fetch_assoc()['username'] != $actualUsername) {
+            $data['error'] = "Der Username muss unique sein.";
+            return $data;
+        } else {
+            // ansonsten werden die Einträge in der Datenbank geupdatet
+            $sqlUpdate = 'UPDATE `users` SET  `vorname` = ?, `nachname` = ?, `adresse` = ?, `plz` = ?, `ort` = ?, `email` = ?, `passwort` = ?, `username` = ? 
+            WHERE `username` = ?';
+
+            $stmtUpdate = $this->dh->db_obj->prepare($sqlUpdate);
+            $stmtUpdate->bind_param(
+                'sssssssss',
+                $data['vorname'],
+                $data['nachname'],
+                $data['adress'],
+                $data['postcode'],
+                $data['city'],
+                $data['email'],
+                $data['pw'],
+                $data['username'],
+                $actualUsername
+            );
+            // Der neue Cookie wird gesetzt
+            if ($stmtUpdate->execute() && $stmtUpdate->affected_rows > 0) {
+                if (isset($_COOKIE['rememberLogin']) && $_COOKIE['rememberLogin']) {
+                    // 30-Tage Cookie
+                    setcookie('rememberLogin', true, time() + (86400 * 30), '/');
+                    setcookie('username', $data['username'], time() + (86400 * 30), '/');
+                    setcookie('admin', $_COOKIE['admin'], time() + (86400 * 30), '/');
+                } else {
+                    // 1-Stune Cookie
+                    setcookie('rememberLogin', true, time() + 3600, '/');
+                    setcookie('username', $data['username'], time() + 3600, '/');
+                    setcookie('admin', $_COOKIE['admin'], time() + 3600, '/');
+                }
+                $data['success'] = 'Daten erfolgreich aktualisisert';
+            } else {
+                $data['error'] = 'Daten bereits aktuell!';
+            }
+            $stmtUpdate->close();
+        }
+        $stmt2->close();
+        return $data;
+    }
+    public function logoutUser()
+    { // wenn der user sich ausloggen will
+        $result = array();
+        // die session $SESSION['username'] wird destroyet
+        if (isset($_SESSION['username'])) {
+            session_destroy();
+            // wir ziehen die Zeit vom cookie ab und somit läuft der cookie ab
+            if (isset($_COOKIE['rememberLogin'])) {
+                setcookie('rememberLogin', '', time() - 3600, '/');
+            }
+            if (isset($_COOKIE['username'])) {
+                setcookie('username', '', time() - 3600, '/');
+            }
+            if (isset($_COOKIE['admin'])) {
+                setcookie('admin', '', time() - 3600, '/');
+            }
+            //logedIn wird auf false gesetzt
+            $result['loggedIn'] = false;
+        }
+        return $result;
+    }
+
+    private function test_input($data)
+    {
+        //zur Datenvalidierung
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
 }
